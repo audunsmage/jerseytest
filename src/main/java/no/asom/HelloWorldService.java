53cfe67f85941de7ec891219ecbc73647d653134
Audun <auduns@gmail.com>
2017-01-25 16:16:29 +0100

    Initial commit

---
+++ src/main/java/no/asom/HelloWorldService.java
@@ -0,0 +1,21 @@
+package no.asom;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+@Path("/")
+public class HelloWorldService {
+
+    @GET
+    @Path("/{param}")
+    public Response getMsg(@PathParam("param") String msg) {
+
+        String output = "Jersey say : " + msg;
+
+        return Response.status(200).entity(output).build();
+
+    }
+
+}
